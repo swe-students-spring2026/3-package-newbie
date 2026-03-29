3d9fac40f2653ba58b1de668fc22085c4d546b0c
wszms384 <2548462107@qq.com>
2026-03-29 17:30:36 +0800

    finish four functions

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import joke_center
 
-print(joke_center.get_joke())
+# Get jokes
+
+print("Programming joke:", joke_center.get_joke("programming"))
+
+# Add a new joke
+
+print(joke_center.add_joke("dad", "I'm afraid for the calendar. Its days are numbered."))
+
+# View categories
+
+print("Categories:", joke_center.list_categories())
+
+# Get random jokes
+
+print("Random joke:", joke_center.get_random_joke())
